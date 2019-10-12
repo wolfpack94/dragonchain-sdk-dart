5f822cfdd0d6e6c0c6ae29441fb4dd53e5d0e66c
Alex Benedetto <alex061994@gmail.com>
2019-10-12 13:09:54 -0700

    added get and set credentials methods

--- dragonchain_sdk/lib/services/credential_service.dart
+++ dragonchain_sdk/lib/services/credential_service.dart
@@ -1,4 +1,5 @@
 library dragonchain_sdk;
+import 'package:dragonchain_sdk/services/config_service.dart';
 
 class CredentialService {
   String dragonchainId;
@@ -12,15 +13,11 @@ class CredentialService {
     {String authKeyId= '', String authKey= '', String hmacAlgo= 'SHA256'}
   ) async {
     if (authKeyId == '' || authKey == '') {
-      // call getCredentials function
-      authKeyId = 'banana';
-      authKey = 'banana';
+      Map<String, String> credentials = await ConfigService.getDragonchainCredentials(dragonchainId);
+      authKeyId = credentials['authKeyId'];
+      authKey = credentials['authKey'];
     }
     var creds = new CredentialService(dragonchainId, { authKeyId: authKeyId, authKey: authKey }, hmacAlgo);
     return creds;
-  }
-
-  static Future<Map<String, String>> getCredentialsFromEnvironment() async {
-    return {};
   }
 }
